03ce57ba17dedcfda9a93b9e234767a07ee0b36e
lukas simianer <lsimianer@gmail.com>
2019-07-08 17:11:10 -0500

    survey made, landing page made. server made. now for swl use

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-var bodyParser = require('body-parser');
-var express = require('express');
+var express = require("express");
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
- 
-app.get('/', function (req, res) {
-  res.send('Hello World, luke is going to be badass developer');
-})
 
-//parse application
-app.use(bodyParser.urlencoded({ extended:false}))
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.json())
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-app.use(function(req,res) {
-    res.setHeader('content-type', 'text-plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-})
- 
-app.listen(PORT, function(){
-    console.log("App listening on PORT: " + PORT);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
-
-
